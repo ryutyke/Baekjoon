832d37c4419454cdb33b8562542e1943d04d0bd8
Seunghyeon Ryu <play2ski@gmail.com>
2023-01-24 22:18:39 +0900

    correct

--- Baekjoon/2920.cpp
+++ Baekjoon/2920.cpp
@@ -3,6 +3,42 @@ using namespace std;
 
 int main()
 {
+	int num, temp;
+	cin >> num;
+
+	if (num == 1)
+	{
+		for (int i = 0; i < 7; i++)
+		{
+			temp = num + 1;
+			cin >> num;
+			if (temp != num)
+				break;
+			if (i == 6)
+			{
+				cout << "ascending";
+				return 0;
+			}
+		}
+	}
+
+	else if (num == 8)
+	{
+		for (int i = 0; i < 7; i++)
+		{
+			temp = num - 1;
+			cin >> num;
+			if (temp != num)
+				break;
+			if (i == 6)
+			{
+				cout << "descending";
+				return 0;
+			}
+		}
+	}
+
+	cout << "mixed";
 
 	return 0;
 }
