cb8095ebbe1457518689ecceb99e499d3ab600ef
Seunghyeon Ryu <play2ski@gmail.com>
2023-01-25 23:24:38 +0900

    correct (longlonglonglonglong;;;;;;;;;)

--- Baekjoon/1654.cpp
+++ Baekjoon/1654.cpp
@@ -2,51 +2,39 @@
 #include<algorithm>
 using namespace std;
 
-int BinarySearch(int* array, int first, int last, short num, int n)
+int main() 
 {
-	int mid, sum, best = 0;
 
-	while (first <= last)
-	{
-		sum = 0;
-		if (last > 1000000) 
-			first = (last / 1000000) - 1;
-		
-		mid = (first + last) / 2;
-
-		for (int i = 0; i < num; i++)
-		{
-			sum += (array[i] / mid);
-		}
-		if (sum >= n)
-		{
-			best = mid;
-			first = mid + 1;
-		}
-		else
-			last = mid - 1;
-	}
-
-	return best;
-}
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
-	short k;
-	int n;
+	int k, n;
+	long long cnt, best = 0;
 	cin >> k >> n;
-	int* array = new int[k];
 
+	int* array = new int[k];
 	for (int i = 0; i < k; i++)
 		cin >> array[i];
 
 	sort(array, array + k);
 
-	cout << BinarySearch(array, 1, array[k - 1], k, n);
+	long long start = 1, end = array[k - 1];
+
+	while (start <= end) 
+	{
+		long long mid = (start + end) / 2;
+		cnt = 0;
+
+		for (int i = 0; i < k; i++) 
+			if (array[i] - mid >= 0) 
+				cnt += array[i] / mid;
+
+		if (cnt >= n) 
+		{
+			start = mid + 1;
+			best = best > mid ? best : mid;
+		}
+		else
+			end = mid - 1;
+	}
+	cout << best;
 
 	return 0;
 }
