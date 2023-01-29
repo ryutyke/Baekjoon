cef3e3a527fa8277b2c9e7451a5ee2a6fb9785df
Seunghyeon Ryu <play2ski@gmail.com>
2023-01-29 17:03:47 +0900

    correct (counting sort)

--- Baekjoon/10989.cpp
+++ Baekjoon/10989.cpp
@@ -8,22 +8,25 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
+	int a[10001] = { 0 };
+	
 	int n;
 	cin >> n;
-
-	short* s = new short[n];
-
+	int temp;
 	for (int i = 0; i < n; i++)
-		cin >> s[i];
-
-	make_heap(s, s + n, greater<>());
-
-	for (int i = n; i >= 1; i--)
 	{
-		pop_heap(s, s + i, greater<>());
-		cout << s[i - 1] << "\n";
+		cin >> temp;
+		a[temp]++;
 	}
-	
+
+	for (int i = 0; i < 10001; i++)
+	{
+		while (a[i] != 0)
+		{
+			cout << i << "\n";
+			a[i]--;
+		}
+	}
 
 	return 0;
 }
