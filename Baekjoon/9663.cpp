f57e910d36299fa68e220b0564e4d7769566f2e0
Seunghyeon Ryu <play2ski@gmail.com>
2023-02-27 15:59:55 +0900

    correct

--- Baekjoon/9663.cpp
+++ Baekjoon/9663.cpp
@@ -1,142 +1,54 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int n;
-int result = 0;
+int N;
+int* ChessBoard;
 
-void dfs(bool** ary, int cnt, int x, int y)
+int Result = 0;
+
+bool Check(int y)
 {
-	if (cnt == n) { result++; }
-	else
+	for (int PastY = 0; PastY < y; PastY++)
 	{
-		bool** CantPut = new bool* [n];
-		for (int i = 0; i < n; i++)
+		if (ChessBoard[y] == ChessBoard[PastY])
 		{
-			CantPut[i] = new bool[n];
+			return false;
 		}
+		if (y - PastY == abs(ChessBoard[y] - ChessBoard[PastY]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
 
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				CantPut[i][j] = ary[i][j];
-			}
-		}
+void PutQueen(int y)
+{
+	if (y == N)
+	{
+		Result++;
+		return;
+	}
 
-		CantPut[y][x] = true;
-		for (int i = 0; i < n; i++)
-		{
-			CantPut[y][i] = true;
-			CantPut[i][x] = true;
-		}
+	for (int x = 0; x < N; x++)
+	{
+		ChessBoard[y] = x;
 
-		int TempX = x;
-		int TempY = y;
-		while (true)
-		{
-			TempX = TempX - 1;
-			TempY = TempY - 1;
-			if (TempX >= 0 && TempY >= 0)
-			{
-				CantPut[TempY][TempX] = true;
-			}
-			else
-			{
-				break;
-			}
-		}
-		TempX = x;
-		TempY = y;
-		while (true)
-		{
-			TempX = TempX + 1;
-			TempY = TempY - 1;
-			if (TempX < n && TempY >= 0)
-			{
-				CantPut[TempY][TempX] = true;
-			}
-			else
-			{
-				break;
-			}
-		}
-		TempX = x;
-		TempY = y;
-		while (true)
-		{
-			TempX = TempX - 1;
-			TempY = TempY + 1;
-			if (TempX >= 0 && TempY < n)
-			{
-				CantPut[TempY][TempX] = true;
-			}
-			else
-			{
-				break;
-			}
-		}
-		TempX = x;
-		TempY = y;
-		while (true)
-		{
-			TempX = TempX + 1;
-			TempY = TempY + 1;
-			if (TempX < n && TempY < n)
-			{
-				CantPut[TempY][TempX] = true;
-			}
-			else
-			{
-				break;
-			}
-		}
-
-
-		for (int i = y; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				if (CantPut[i][j] == false)
-				{
-					dfs(CantPut, cnt+1, j, i);
-					CantPut[i][j] = true;
-				}
-			}
-		}
-
-		for (int i = 0; i < n; i++)
-		{
-			delete[] CantPut[i];
-		}
-		delete[] CantPut;
+		if (Check(y))
+			PutQueen(y + 1);
 	}
 }
 
 int main()
 {
 	ios::sync_with_stdio(false); cin.tie(0);
-	
-	cin >> n;
-	bool** CantPut = new bool* [n];
-	for (int i = 0; i < n; i++)
-	{
-		CantPut[i] = new bool[n];
-		for (int j = 0; j < n; j++)
-		{
-			CantPut[i][j] = 0;
-		}
-	}
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			dfs(CantPut, 1, j, i);
-			CantPut[i][j] = true;
-		}
-	}
-	
-	cout << result;
+	cin >> N;
+	ChessBoard = new int[N];
+
+	PutQueen(0);
+
+	cout << Result;
 
 	return 0;
 }
